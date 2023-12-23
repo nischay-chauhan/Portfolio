8485d84738a0425a1677ad657d58f22db242c7e3
nischay-chauhan <masternischay@outlook.com>
2023-12-23 15:24:11 +0530

    initial aboutSection

--- portfolio/portfolio/app/page.js
+++ portfolio/portfolio/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
+import AboutSection from './components/AboutSection';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Navbar />
     <div className='container mt-24 mx-auto  px-4 py-12'>
       <HeroSection />
+      <AboutSection />
       </div>
     </main>
   );
